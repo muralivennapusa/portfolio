ecaf242d47c2f50ae70923d04e6e8e61a970cc92
Quincy Thawne <quincythawne@gmail.com>
2025-06-30 19:58:35 +0530

    nav bar mods

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -3,21 +3,23 @@ import { Moon, Sun } from 'lucide-react';
 
 export default function TopNavbar({ darkMode, toggleDarkMode }) {
   return (
-    <header className="top-navbar">
-      <div className="nav-left">
-        <a href="#" className="logo">Murali Vennapusa</a>
-        <nav>
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#socials">Socials</a>
-        </nav>
-      </div>
+<header className="top-navbar">
+  <div className="nav-left">
+    <a href="#" className="logo">Murali Vennapusa</a>
+    <nav>
+      <a href="#about">About</a>
+      <a href="#projects">Projects</a>
+      <a href="#socials">Socials</a>
+    </nav>
+  </div>
 
-      <div className="theme-toggle">
-        <button onClick={toggleDarkMode} className="theme-icon-btn" aria-label="Toggle Theme">
-          {darkMode ? <Moon size={22} /> : <Sun size={22} />}
-        </button>
-      </div>
-    </header>
+  <div className="theme-toggle">
+    <button onClick={toggleDarkMode} className="theme-icon-btn" aria-label="Toggle Theme">
+      {darkMode ? <Moon size={22} /> : <Sun size={22} />}
+    </button>
+  </div>
+</header>
+
+
   );
 }
