57a3c82038f30ea45d9bcbe7cf64737b1f201e12
Quincy Thawne <142000861+QuincyThawne@users.noreply.github.com>
2025-06-30 16:10:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:true,
-  },
+  resolve: {
+    alias: {
+      'react-slick': 'react-slick',
+      'slick-carousel': 'slick-carousel'
+    }
+  }
 })
