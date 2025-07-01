4a0206e843031d3f4a4d5663e3a6a9fe164a1d0f
Quincy Thawne <quincythawne@gmail.com>
2025-07-01 11:52:31 +0530

    background animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer id="footer">
-      <div className="social-icons">
+      {/* <div className="social-icons">
         <a href="https://github.com/muralivennapusa" target="_blank"><FaGithub /></a>
         <a href="https://linkedin.com/in/vennapusa-murali" target="_blank"><FaLinkedin /></a>
         <a href="https://twitter.com/muralireddyon" target="_blank"><FaTwitter /></a>
-      </div>
+      </div> */}
       <h4><u>P.S:</u>  GGs</h4>
       <p>from Murali Vennapusa</p>
     </footer>
